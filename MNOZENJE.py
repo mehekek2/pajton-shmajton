bce74c51e47e4b95561626ed96729ac7a1e6f008
mehekek <bornajelic@hotmail.com>
2017-05-17 15:22:05 +0200

    Update MNOZENJE.py
    Dodao sam komentar za x matricu, popravio dimenzije matrice za result2

--- MNOZENJE.py
+++ MNOZENJE.py
@@ -1,4 +1,4 @@
-
+# 3x3 matrica
 X = [[12,7,3],
     [4 ,5,6],
     [7 ,8,9]]
@@ -7,14 +7,15 @@ Y = [[5,8,1,2],
     [6,7,3,0],
     [4,5,9,1]]
 # result JE 3x4 matrica
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
 
-result2 = [[0,0,0], 
-       	  [0,0,0]]
 # iteracija po petljama i i j
 for i in range(len(X)):
    
    for j in range(len(Y[0])):
-       # iteracija po dodatnoj pelji k koju koristimo da mijenjamo "smijer"
+      
        for k in range(len(Y)):
            result[i][j] += X[i][k] * Y[k][j]
 for r in result:
