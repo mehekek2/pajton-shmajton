ce154cab2114046d65c5a18adf5ecf51d752121b
mehekek2 <batafljeska@gmail.com>
2017-05-17 18:23:43 +0200

    Update MNOZENJE.py
    Dodao sam program koji daje transponiranu matricu X

--- MNOZENJE.py
+++ MNOZENJE.py
@@ -1,23 +1,36 @@
-# 3x3 matrica
-# ova prezentacija je ulozila dosta truda :)
+
 X = [[12,7,3],
     [4 ,5,6],
     [7 ,8,9]]
-# 3x4 matrix
+
 Y = [[5,8,1,2],
     [6,7,3,0],
     [4,5,9,1]]
-# result JE 3x4 matrica
+
 result = [[0,0,0,0],
          [0,0,0,0],
          [0,0,0,0]]
+#result2 tu se sprema transponirana matrica
+result2 = [[0,0,0], 
+       	  [0,0,0]]
 
-# iteracija po petljama i i j
 for i in range(len(X)):
    
    for j in range(len(Y[0])):
-      #dodatna petlja koja omogucuje ''okomitu'' iteraciju
+       
        for k in range(len(Y)):
            result[i][j] += X[i][k] * Y[k][j]
+print(" ")
+for r in result:
+   print(r)
+
+
+#==========TRANSPONIRANJE===================
+print("TRANSPONIRANA")
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[j][i] = X[i][j]
+print(" ")
 for r in result:
    print(r)
