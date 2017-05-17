70af0544b6cb2f1281498edb1a111bbead0eb941
mehekek <bornajelic@hotmail.com>
2017-05-17 15:03:28 +0200

    Update rijesavanje.py
    Izbacio sam nepotrebne komentare

--- rijesavanje.py
+++ rijesavanje.py
@@ -1,4 +1,4 @@
-
+#matrica X
 X = [[12,7,3],
     [4 ,5,6],
     [7 ,8,9]]
@@ -15,9 +15,9 @@ result2 = [[0,0,0],
        	  [0,0,0]]
 # iterate through rows of X	
 for i in range(len(X)):
-   # iterate through columns of Y
+ 
    for j in range(len(Y[0])):
-       # iterate through rows of Y
+    
        for k in range(len(Y)):
            result[i][j] += X[i][k] * Y[k][j]
 for r in result:
